9d272e297d33f7c819e4601ff40920750278dd91
acourtin <acourtin@student.42.fr>
2017-11-23 17:14:15 +0100

    added begining of function

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,19 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 15:04:13 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/23 15:04:53 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/23 17:13:52 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 int				get_next_line(const int fd, char **line)
 {
+	char			c;
+	static int		bufpos = 0;
+	int				buffer;
+
+	buffer = read(fd, &c, 1);
+	close(fd);
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,8 +6,16 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 15:04:59 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/23 15:05:28 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/23 17:13:55 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# define BUFF_SIZE 32
+
 int				get_next_line(const int fd, char **line);
+
+#endif
