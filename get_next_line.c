39ceba1191d1b3637c76d21e974c0b83b447bdb7
acourtin <acourtin@student.42.fr>
2017-11-25 18:42:56 +0100

    it lives git add *! kind of

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,26 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 15:04:13 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/25 16:45:11 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/25 18:37:04 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "./Libft/libft.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
+#include <string.h>
 
 int				get_next_line(const int fd, char **line)
 {
 	static int		bufpos = 0;
+	static char		reste[BUFF_SIZE + 1] = "\0";
 	char			*c;
 	int				buffer;
 	int				i;
+	int				j;
+	int				k;
 	char			*str;
 	int				write;
 
@@ -31,26 +37,57 @@ int				get_next_line(const int fd, char **line)
 	if (!line)
 		return (-1);
 	str = *line;
+	c = ft_strnew(BUFF_SIZE + 1);
 	buffer = read(fd, c, BUFF_SIZE);
+	printf("buffer = %i\n", buffer);
+	if (buffer == -1)
+	{
+		printf("errno = %s\n", strerror(errno));
+		return (-1);
+	}
+	j = 0;
+	while (reste[j])
+	{
+		str[j] = reste[j];
+		j++;
+	}
+	i = 0;
+	while (c[i] == '\n')
+		i++;
 	if (buffer > 0)
 	{
-		i = 0;
-		while (i < BUFF_SIZE && write == 1)
+		while (write == 1 && c[i] != '\0')
 		{
-			if (c[i] != '\0')
+			if (i >= BUFF_SIZE - 1)
 			{
-				if (c[i] == '\n')
-				{
-					str[i] = '\0';
-					bufpos = i;
-					write = 0;
-				}
-				else
-					str[i] = c[i];
+				buffer = read(fd, c, BUFF_SIZE);
+				i = 0;
 			}
-			i++;
+			if (c[i] == '\n' || c[i] == '\0')
+			{
+				str[j] = '\0';
+				bufpos += i;
+				write = 0;
+			}
+			else
+			{
+				str[j] = c[i];
+				i++;
+				j++;
+			}
 		}
-		str[i] = '\0';
+		while (c[i] == '\n')
+			i++;
+		k = 0;
+		while (c[i] != '\0' && c[i] != '\n')
+		{
+			reste[k] = c[i];
+			i++;
+			k++;
+		}
+		//printf("reste = %s\n", reste);
+		i = 0;
+		str[j] = '\0';
 		if (write == 1)
 			bufpos += BUFF_SIZE;
 		printf("bufpos = %i\n", bufpos);
--- maino.c
+++ maino.c
@@ -6,11 +6,12 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 17:37:31 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/25 16:41:05 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/25 17:44:16 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "./Libft/libft.h"
 #include <stdlib.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -25,12 +26,14 @@ int			main(int ac, char **av)
 
 	if (ac == 2)
 	{
-		line = (char*)malloc(sizeof(char) * (BUFF_SIZE + 1));
+		line = ft_strnew(BUFF_SIZE + 1);
 		fd = open(av[1], O_RDONLY);
 		while ((gnl = get_next_line(fd, &line)) == 1)
 		{
-			printf("%s", line);
+			printf("line = %s", line);
 			printf("\n");
+			free(line);
+			line = ft_strnew(BUFF_SIZE + 1);
 		}
 		if (gnl == -1)
 			printf("GNL failed\n");
