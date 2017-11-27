0880efb5e1a230054d3cf1e29b601b1ef5ce9176
acourtin <acourtin@student.42.fr>
2017-11-27 21:35:17 +0100

    cleanup

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: acourtin <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/23 15:04:13 by acourtin          #+#    #+#             */
-/*   Updated: 2017/11/26 17:40:19 by acourtin         ###   ########.fr       */
+/*   Updated: 2017/11/27 21:24:57 by acourtin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,84 +17,61 @@
 #include <errno.h>
 #include <string.h>
 
-static int		remalloc(char **remain)
+int				remalloc(char **save)
 {
 	char *tmp;
 
-	if (!(*remain))
+	tmp = ft_strdup(*save);
+	ft_strdel(save);
+	if (!(*save = ft_strnew(ft_strlen(tmp) + BUFF_SIZE)))
 		return (0);
-	tmp = ft_strnew(ft_strlen(*remain));
-	ft_strcpy(tmp, *remain);
-	*remain = NULL;
-	*remain = ft_strnew(ft_strlen(tmp) + BUFF_SIZE);
-	ft_strcpy(*remain, tmp);
+	*save = ft_strcpy(*save, tmp);
+	ft_strdel(&tmp);
 	return (1);
 }
 
-static int		to_line(int buffer, char **remain, char **line)
+int				process(char **save, char **line)
 {
-	int i;
-	int j;
+	unsigned long	i;
+	int				write;
 
 	i = 0;
-	j = 0;
-	if (buffer != 0 || ft_strlen((*remain)) != 0)
+	write = 0;
+	ft_strdel(line);
+	*line = ft_strdup(*save);
+	ft_strdel(save);
+	if ((*line)[0] == '\0')
+		return (0);
+	while ((*line)[i])
 	{
-		while ((*remain)[i] != '\n')
+		if ((*line)[i] == '\n')
 		{
-			(*line)[i] = (*remain)[i];
-			i++;
+			(*line)[i] = '\0';
+			*save = ft_strdup((*line) + i + 1);
+			break ;
 		}
-		(*line)[i] = '\0';
 		i++;
-		if ((*remain)[0] == '\n')
-		{
-			while ((*remain)[i])
-			{
-				(*remain)[j] = (*remain)[i];
-				i++;
-				j++;
-			}
-			(*remain)[j] = '\0';
-			return (1);
-		}
-		while ((*remain)[i])
-		{
-			(*remain)[j] = (*remain)[i];
-			i++;
-			j++;
-		}
-		(*remain)[j] = '\0';
 	}
-	return (0);
+	return (1);
 }
 
 int				get_next_line(const int fd, char **line)
 {
-	static char	*remain;
-	int			buffer;
+	static char	*save;
+	int			b;
 
-	if ((!remain && !(remain = ft_strnew(BUFF_SIZE))) || !(*line = ft_strnew(BUFF_SIZE)) || fd < 0)
+	if (!line || (!save && !(save = ft_strnew(BUFF_SIZE * 2))) \
+			|| !(*line = ft_strnew(BUFF_SIZE)) || fd < 0)
 		return (-1);
-	while ((buffer = read(fd, *line, BUFF_SIZE)) > 0)
+	while ((b = read(fd, *line, BUFF_SIZE)) != 0)
 	{
-		if (remalloc(&remain) == 0)
+		if (remalloc(&save) == 0 || b < 0)
 			return (-1);
-		ft_strncat(remain, *line, BUFF_SIZE);
-		if (ft_memchr(*line, '\n', BUFF_SIZE))
+		save = ft_strncat(save, *line, b);
+		if (ft_memchr(save, '\n', BUFF_SIZE))
 			break ;
 	}
-	//printf("line = %s\nremain = %s\n", *line, remain);
-	if (to_line(buffer, &remain, line) == 1)
-	{
+	if (process(&save, line))
 		return (1);
-	}
-	//printf("line = %s\nremain = %s\n", *line, remain);
-	if (ft_memcmp(*line, remain, ft_strlen(*line)) == 0)
-	{
-		if (!(*line = ft_strdup("")))
-			return (1);
-		return (0);
-	}
-	return (1);
+	return (0);
 }
